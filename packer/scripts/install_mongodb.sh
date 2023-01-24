76de447ead402e755e2e25205975b93687762a6b
Sachkov Dmitriy <avallakittishvarra@gmail.com>
2023-01-24 13:39:52 +0300

    hw9 done

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock*
+sudo dpkg --configure -a
 wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - # После пайпа sudo не убрано - а то не отработает добавление ключа
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock*
+sudo dpkg --configure -a
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
