956e3714edd697e02f666ed960ba8319c6c5511b
Sachkov Dmitriy <avallakittishvarra@gmail.com>
2023-02-05 14:10:54 +0300

    default vars values added

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -1,10 +1,12 @@
-variable public_key_path {
+variable "public_key_path" {
   description = "Path to the public key used for ssh access"
+  default     = "~/.ssh/id_rsa.pub"
 }
-variable app_disk_image {
+variable "app_disk_image" {
   description = "Disk image for reddit app"
-  default = "reddit-app-base"
+  default     = "reddit-app-base"
 }
-variable subnet_id {
-description = "Subnets for modules"
+variable "subnet_id" {
+  description = "Subnets for modules"
+  default     = "e9bm9qtiqc3gfrm7c1rl"
 }
